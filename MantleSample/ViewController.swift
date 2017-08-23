baa208992483612b4d18381d36802af9c3f16946
Emre Özdil <emre.ozdil@inomera.com>
2017-08-23 11:55:16 +0300

    Initial Commit

---
+++ MantleSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MantleSample
+//
+//  Created by Emre Özdil on 23/08/2017.
+//  Copyright © 2017 Emre Özdil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
